8c765fccbed57ae4085f768fdc2c4507a76b1f00
LizaDmit <edmitrienko@connect.ust.hk>
2026-01-26 15:18:23 +0800

    Addition of vec3.h and color.h to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -17,15 +17,8 @@ int main() {
 
         for (int i = 0; i < image_width; i++) {
 
-            double r = double(i) / (image_width-1);        // Color on [0,1] scale
-            double g = double(j) / (image_height-1);       
-            double b = 0.0;
-
-            int ir = int(255.999 * r);                     // Color on [0,255] scale
-            int ig = int(255.999 * g);
-            int ib = int(255.999 * b);
-
-            cout << ir << ' ' << ig << ' ' << ib << '\n';
+            auto pixel_color = color(double(i)/(image_width-1), double(j)/(image_height-1), 0);
+            write_color(cout, pixel_color);
         }
     }
     clog << "\rDone.                 \n";
