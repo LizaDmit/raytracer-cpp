26aef9a2da676f863bb977819d100850b8fc92ae
LizaDmit <edmitrienko@connect.ust.hk>
2026-01-26 15:38:03 +0800

    Definition of ray class

---
+++ src/ray.h
@@ -0,0 +1,30 @@
+#ifndef RAY_Y
+#define RAY_H
+
+#include "vec3.h"
+using namespace std;
+
+class ray {
+
+    public:
+
+        ray() {}                                                                           // Empty constructor
+
+        ray(const point3& origin, const vec3& direction) : orig(origin), dir(direction) {} // Constructor
+
+        const point3& origin() const  { return orig; }                                     // Gettters
+        const vec3& direction() const { return dir; }
+
+
+        point3 at(double t) const {                                                        // Finds where the vector points
+            return orig + t*dir;} 
+
+    private:
+        point3 orig;
+        vec3 dir;
+
+};
+
+
+
+#endif
